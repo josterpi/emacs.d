1f8f7e223a85794c228cac681451d7b1b567b71f
Jeremy Osterhouse <josterhouse@gmail.com>
2020-04-17 13:34:30 -0400

    Add a TODO

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
 
+;; TODO: When I C-x C-c with server running, it keeps all my buffers open
+;; https://stackoverflow.com/questions/1167484/how-to-gracefully-shutdown-emacs-daemon
+
 (defvar mswindows-p (string-match "windows" (symbol-name system-type)))
 (defvar linux-p (string-match "linux" (symbol-name system-type)))
 
